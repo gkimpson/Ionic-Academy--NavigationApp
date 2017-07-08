32371503b8953f7939df1601fdb7f9263885fa8e
Gavin.Kimpson <gkimpson@gmail.com>
2017-07-08 21:03:05 +0100

    Building a Tab Bar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { ThirdPage } from './../third/third';
 import { SecondPage } from './../second/second';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
@@ -7,9 +8,12 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  tab1: any;
+  tab2: any;
 
   constructor(public navCtrl: NavController) {
-
+    this.tab1 = SecondPage;
+    this.tab2 = ThirdPage;
   }
 
   goToNext() {
